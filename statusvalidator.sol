186030d61ac8ec9beb364922a19faf6ff094dfe5
Andrés Rozo <80934853+rozoandrescamilo@users.noreply.github.com>
2023-03-09 11:05:03 -0500

    Update statusvalidator.sol

--- statusvalidator.sol
+++ statusvalidator.sol
@@ -12,34 +12,18 @@ contract StatusValidator {
     // Un enumerable llamado Status que define los diferentes estados posibles de un paso en el flujo de proceso
     enum Status {
         TO_BE_CONFIRMED, 
-        APPROVED, 
-        BOOKING_REQUEST 
-    }
-    
-    enum PurchaseOrderTypeB {
-        TO_BE_CONFIRMED, 
-        APPROVED_P1, 
-        TO_BE_CONFIRMED_P2, 
-        APPROVED_P2, 
-        APPROVED, 
-        BOOKING_REQUEST 
-    }
-
-    enum PurchaseOrderTypeC {
-        TO_BE_CONFIRMED, 
         APPROVED_P1, 
         TO_BE_CONFIRMED_P2, 
         APPROVED_P2, 
         TO_BE_CONFIRMED_P3, 
-        APPROVED_P3, 
-        APPROVED, 
+        APPROVED_P3,
         BOOKING_REQUEST 
-    }  
+    } 
 
     // Un evento llamado RegisteredStep que se activa cuando una PO se registra en el flujo 
     event RegisteredStep(
         uint256 POID, // ID del producto
-        uint256 poType,
+        uint256 poType, // Tipo de orden: Entrega total, 2 parciales o 3 parciales
         Status status, // Estado del paso
         string metadata, // Metadatos adicionales
         address author // Dirección del autor
@@ -49,19 +33,20 @@ contract StatusValidator {
     mapping(uint256 => Step[]) public POvalidator;
 
     // Una función para registrar una PO que contendrá steps.
-    function registerPO(uint256 POID, uint256 poType) public returns (bool success) {
+    function registerPO(uint256 POID) public returns (bool success) {
         // Comprueba que la PO no haya sido registrado previamente.
         require(POvalidator[POID].length == 0, "This product already exists");
-        require(poType == 1 || poType == 2 || poType == 3, "Invalid PO type");
         // Agrega un paso inicial al producto con el estado "TO_BE_CONFIRMED".
         POvalidator[POID].push(Step(Status.TO_BE_CONFIRMED, ""));        
         return success;
     }
 
-    // Una función para registrar un nuevo paso en el flujodel proceso.
-    function registerStep(uint256 POID, string calldata metadata, uint256 poType) public returns (bool success){
-        // Comprueba que la PO haya sido registrado previamente.
+    // Una función para registrar un nuevo paso en el flujo del proceso.
+    function registerStep(uint256 POID, string calldata metadata, uint256 poType) public returns (bool success) {
+         // Comprueba que la PO haya sido registrado previamente.
         require(POvalidator[POID].length > 0, "This Purchase Order doesn't exist");
+         // Comprueba que los tipos orden sean los correctos.
+        require(poType == 1 || poType == 2 || poType == 3, "Invalid PO type");
         // Obtiene la matriz de pasos actual para el producto.
         Step[] memory stepsArray = POvalidator[POID];
         // Calcula el estado siguiente para el paso actual.
@@ -69,6 +54,18 @@ contract StatusValidator {
         // Dado el caso de que el status sea mayor a COMPLETED envía error
         if (currentStatus > uint256(Status.BOOKING_REQUEST)) {
             revert("The Purchase Order has no more steps");
+        }
+        // Dado el tipo de estado 1 se da el máximo estado posible 2
+        if (poType == 1 && currentStatus > 2) {
+            revert("The maximum status for poType 1 is 2");
+        }
+        // Dado el tipo de estado 2 se da el máximo estado posible 4
+        if (poType == 2 && currentStatus > 4) {
+            revert("The maximum status for poType 2 is 4");
+        }
+        // Dado el tipo de estado 3 se da el máximo estado posible 6
+        if (poType == 3 && currentStatus > 6) {
+            revert("The maximum status for poType 3 is 6");
         }
         // Se asigna el estado actual + 1 y se añade al mapping de PO
         Step memory step = Step(Status(currentStatus), metadata);
